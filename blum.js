0e9bcc9beb63bbaafa2cdd70c7ac0acf1d3e5c5c
sapoarys <a.sabyrzhan@gmail.com>
2024-12-24 19:45:58 +0500

    Create blum.js

---
+++ blum.js
@@ -0,0 +1,34 @@
+// ==UserScript== 
+// @name        blum/main
+// @namespace   Violentmonkey Scripts
+// @match       https://telegram.blum.codes/*
+// @grant       none
+// @version     3
+// @description 15.12.2024, 08:52:00
+
+// ==/UserScript==
+
+// Флаг для отслеживания статуса выполнения скрипта
+let isFirstTabClicked = false;
+let isSecondTabClicked = false;
+
+// Функция ожидания
+function wait(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// Ждем, пока первая вкладка появится и выполняем клик
+const waitForFirstTab = setInterval(async () => {
+    if (isFirstTabClicked) {
+        clearInterval(waitForFirstTab); // Останавливаем проверку, если клик уже выполнен
+        return;
+    }
+
+    const firstTabElement = document.querySelector('.tab');
+    if (firstTabElement) {
+        clearInterval(waitForFirstTab); // Останавливаем проверку
+        firstTabElement.click(); // Кликаем по первой вкладке
+        isFirstTabClicked = true; // Устанавливаем флаг выполнения
+        console.log("Первая вкладка была нажата.");
+    }
+}, 10000); // Проверяем каждые 10000 мс
